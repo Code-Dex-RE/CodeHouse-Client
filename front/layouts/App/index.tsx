df87ae4c46ced83b92003a2c712a50b85db7114e
wjdrk70 <58238859+wjdrk70@users.noreply.github.com>
2021-04-19 01:42:08 +0900

    mockServer 및 회원가입layout 목서버로그인 작업

--- front/layouts/App/index.tsx
+++ front/layouts/App/index.tsx
@@ -7,14 +7,13 @@ const Login = loadabel(() => import('@pages/Login'));
 const SignUp = loadabel(() => import('@pages/SignUp'));
 const Join = loadabel(() => import('@pages/Join'));
 
-
 const App = () => (
   <Switch>
     <Route path="/login" component={Login} />
     <Route path="/signup" component={SignUp} />
     <Route path="/join" component={Join} />
     <Route exact path="/" component={Home} />
-
+  </Switch>
 );
 
 export default App;
